a4093e93e5e761c6d2e6bd25603a7bc2dad8b067
Nibir Bora <nibirbora@gmail.com>
2023-12-03 22:09:59 -0800

    Bootstrap homepage using NextJS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My personal website
+
+Run `npm run dev` and navigate to [http://localhost:3000](http://localhost:3000) for local development.
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/nbir.github.io",
+    images: {
+        unoptimized: true,
+    },
+}
+
+module.exports = nextConfig
