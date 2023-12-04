8b695f16e71fe0e19c6abcd70a277e8b45f3f18b
Nibir Bora <nibirbora@gmail.com>
2023-12-03 22:15:20 -0800

    Deleted files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/nbir.github.io",
+    // basePath: "/nbir.github.io",
     images: {
         unoptimized: true,
     },
